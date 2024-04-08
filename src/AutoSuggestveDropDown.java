f26e78ee546229920073de35ef922f895d40ef84
unknown <shyamgupta1.nmims@gmail.com>
2024-04-08 15:50:16 +0530

    Auto suggestive dropdown code

---
+++ src/AutoSuggestveDropDown.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutoSuggestveDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		 WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(3000);
+//		List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item']//a"));
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		
+		for(WebElement option : options) {
+			if(option.getText().equalsIgnoreCase("India")) {
+				option.click();
+				break;
+			}
+		}
+		
+
+	}
+
+}
---
+++ src/ReadingExcel.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+//import com.google.common.collect.Table.Cell;
+
+public class ReadingExcel {
+
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+		// TODO Auto-generated method stub
+		
+		//In this we take the file which i have to read
+		File f = new File("C:\\Users\\Sumit\\Desktop\\Selenium Jars\\filewriting\\myExcelFile.xlsx");
+		
+		//In this we load the above file -->
+        FileInputStream fi = new FileInputStream(f);
+        
+        
+        // Creating the connection with the above file-->
+        Workbook workbook = WorkbookFactory.create(fi);
+        
+        Sheet sheet0 = workbook.getSheetAt(0);
+        
+        for(Row row : sheet0) {
+        	
+        	for(Cell cell: row) {
+        		
+        		switch(cell.getCellType()) 
+        		{
+        		case STRING:
+        			System.out.print(cell.getStringCellValue()+ "\t");
+        			break;
+        		case NUMERIC:
+        			System.out.print(cell.getNumericCellValue()+ "\t");
+        			break;
+        		case BOOLEAN: 
+        			System.out.print(cell.getBooleanCellValue()+ "\t");
+        			break;
+        		case BLANK:
+        			System.out.print("Blank cell"+"\t");
+				default:
+					break;
+        		}
+        		
+        	}
+        	System.out.println();
+        }
+        
+	}
+
+}
