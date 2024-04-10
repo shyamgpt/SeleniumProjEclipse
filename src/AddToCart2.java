e4b0f6d0f2e451235da187672b6ae47bdb292286
unknown <shyamgupta1.nmims@gmail.com>
2024-04-10 17:30:21 +0530

    Action class

--- src/AddToCart2.java
+++ src/AddToCart2.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -8,28 +9,55 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class AddToCart1 {
+public class AddToCart2 {
 
 	@Test
-	public static void Cart2() throws InterruptedException {
+	public static void AddindToCart() throws InterruptedException {
 //		"//button[text()='ADD TO CART']"
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		
+//		int j =0;
+		String[] itemNeeded = {"Cucumber", "Brocolli", "Beetroot"};
+		
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-
-		for (int i = 0; i < products.size(); i++) {
-			String name = products.get(i).getText();
-			System.out.println(name);
-			if (name.contains("Beetroot")) {
+		
+		for(int i =0; i<products.size(); i++) {
+			
+			//Cucumber - 1 Kg  --> Cucumber , 1 Kg
+			
+			String[] name = products.get(i).getText().split("-");
+			String formattedName = name[0].trim();
+			System.out.println(formattedName);
+			
+			List itemNeededList = Arrays.asList("itemNeeded");
+			
+			
+			
+			
+			
+			
+			if(itemNeededList.contains(formattedName)) {
+				Thread.sleep(3000);
+				
+//				j++;
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-
-				break;
+				System.out.println(formattedName);
+				
+				
+				
+//				if(j ==3) {
+//					break;
+//				}
 			}
+			
 		}
+
+	
 
 	}
 
---
+++ src/AjaxIFrameChildWindow/ActionDemo.java
@@ -0,0 +1,43 @@
+package AjaxIFrameChildWindow;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ActionDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+
+		Thread.sleep(2000);
+
+		WebElement move = driver.findElement(By.cssSelector("span[id='nav-link-accountList-nav-line-1']"));
+
+		Actions a = new Actions(driver);
+
+		a.moveToElement(move).build().perform();
+		
+        // This will sendkeys the word in capital letter-->
+//		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("Hello")
+//				.build().perform();
+		
+		//This will double click the send letters-->
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("Hello").doubleClick()
+		.build().perform();
+		
+		//Right click on specific element -->
+		a.moveToElement(move).contextClick().build().perform();
+
+	}
+
+}
