12ab64057eac636d8988dc74d502c2faf543565d
unknown <shyamgupta1.nmims@gmail.com>
2024-04-13 16:35:36 +0530

    Set Proxies, download path location

--- src/MiscellaneousTopicsInSelenium/BlockPopWindow.java
+++ src/MiscellaneousTopicsInSelenium/BlockPopWindow.java
@@ -1,29 +1,26 @@
 package MiscellaneousTopicsInSelenium;
 
+import java.util.Arrays;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class handlingHTTPScertification {
+public class BlockPopWindow {
 
 	public static void main(String[] args) {
-		
-		
-//		FirefoxOptions option = new FirefoxOptions();
-//		option.setAcceptInsecureCerts(true);
+		// TODO Auto-generated method stub
 		
 		ChromeOptions options = new ChromeOptions();
-		options.setAcceptInsecureCerts(true);
-	
+		options.setExperimentalOption("excludeSwitches",
+				Arrays.asList("disable-popup-blocking"));
+		
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver(options);
-		driver.get("https://expired.badssl.com/");
-		
-		System.out.println(driver.getTitle());
+		driver.get("https://www.spicejet.com/");
 
 	}
 
---
+++ src/MiscellaneousTopicsInSelenium/SetDownloadDirectory.java
@@ -0,0 +1,10 @@
+package MiscellaneousTopicsInSelenium;
+
+public class SetDownloadDirectory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
