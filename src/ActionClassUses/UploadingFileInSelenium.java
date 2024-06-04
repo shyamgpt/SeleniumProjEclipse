7017a7a5677e27fae38e87116909f09e12bbf8b9
Shyam Gupta <Shyamgupta1.nmims@gmail.com>
2024-06-04 21:52:04 +0530

    Action class

---
+++ src/ActionClassUses/UploadingFileInSelenium.java
@@ -0,0 +1,10 @@
+package ActionClassUses;
+
+public class UploadingFileInSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ActionClassUses/keyBoardAction.java
@@ -0,0 +1,35 @@
+package ActionClassUses;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class keyBoardAction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		Actions act = new Actions(driver);
+		driver.findElement(By.name("firstname")).sendKeys("Selenium");
+		
+		act.sendKeys(Keys.TAB)
+		.pause(Duration.ofSeconds(0))
+		.sendKeys("Webdriver")
+		.pause(Duration.ofSeconds(0))
+		.sendKeys("7328973297")
+		.sendKeys(Keys.TAB)
+		.build().perform();
+		
+
+	}
+
+}
